60a6b489e95c04c9deeac6a1e028be5fef87d9bd
Nicolas Olivares <nolivaresr@hotmail.com>
2020-08-31 20:58:27 -0400

    first commit

---
+++ db/init_db.js
@@ -0,0 +1,44 @@
+// code to build and initialize DB goes here
+const {
+	client,
+	// other db methods
+} = require('./index');
+
+async function dropTables() {
+	try {
+		await client.query(`
+      DROP TABLE IF EXISTS links_tags; 
+    `);
+	} catch (error) {
+		throw error;
+	}
+}
+
+async function createTables() {
+  
+}
+
+async function buildTables() {
+	try {
+		client.connect();
+
+		// drop tables in correct order
+
+		// build tables in correct order
+	} catch (error) {
+		throw error;
+	}
+}
+
+async function populateInitialData() {
+	try {
+		// create useful starting data
+	} catch (error) {
+		throw error;
+	}
+}
+
+buildTables()
+	.then(populateInitialData)
+	.catch(console.error)
+	.finally(() => client.end());
