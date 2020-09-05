8a48746bb903402c2936fd8fb0e7cc0672b0276e
causey904.bc <your_email@real_email.edu>
2020-09-04 21:16:39 -0400

    adding server setup and links route.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const apiRouter = require('express').Router();
-
+const linksRouter = require('./links');
 apiRouter.get("/", (req, res, next) => {
   res.send({
     message: "API is under construction!"
   });
 });
+apiRouter.use('/links', linksRouter)
 
 module.exports = apiRouter;
