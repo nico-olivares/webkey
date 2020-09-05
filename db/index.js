e2ae17bc8dfd54376ebdbd77bde8b32a1232ad34
coyotewind <coyoteswind@gmail.com>
2020-09-05 18:23:59 -0400

    commits fix for post-links route

--- db/index.js
+++ db/index.js
@@ -1,11 +1,11 @@
 // sets up DB
 
 const { Client } = require('pg');
-const DB_NAME = 'webkey'
+const DB_NAME = 'webkey';
 
 const DB_URL = process.env.DATABASE_URL || `postgres://localhost:5432/webkey`;
 const client = new Client(DB_URL);
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 
 // database methods
 
@@ -14,96 +14,116 @@ async function getAllUsers() {
     return rows;
 }
 
-// goal: create a new user that can be validated 
-// input: takes in username and password 
+// goal: create a new user that can be validated
+// input: takes in username and password
 // outp
 
 async function createUser({ username, password }) {
-  try {
-    const { rows: [user] } = await client.query(`
+    try {
+        const {
+            rows: [user],
+        } = await client.query(
+            `
     INSERT INTO users(username, password)
     VALUES ($1, $2)
     ON CONFLICT (username) DO NOTHING 
     RETURNING *;
-  `, [username, password])
-    return user
-  } catch (error) {
-    throw (error)
-  }
-
+  `,
+            [username, password]
+        );
+        return user;
+    } catch (error) {
+        throw error;
+    }
 }
 
 async function getUserByUsername({ username }) {
-  try {
-    const { rows: [user] } = await client.query(`
+    try {
+        const {
+            rows: [user],
+        } = await client.query(
+            `
           SELECT *
           FROM users
           WHERE username=$1;
-      `, [username]);
-    if (!user || user.length === 0) {
-      return null
+      `,
+            [username]
+        );
+        if (!user || user.length === 0) {
+            return null;
+        }
+        return user;
+    } catch (error) {
+        throw error;
     }
-    return user
-  } catch (error) {
-    throw error
-  }
 }
 
 async function getUserById(id) {
-  try {
-    const { rows: [user] } = await client.query(`
+    try {
+        const {
+            rows: [user],
+        } = await client.query(
+            `
           SELECT *
           FROM users
           WHERE id=$1;
-      `, [id]);
-    if (!user || user.length === 0) {
-      return null
+      `,
+            [id]
+        );
+        if (!user || user.length === 0) {
+            return null;
+        }
+        return user;
+    } catch (error) {
+        throw error;
     }
-    return user
-  } catch (error) {
-    throw error
-  }
 }
+
 async function getUser({ username, password }) {
-  try {
-    const user = await getUserByUsername({ username });
-    console.log('user', user)
-    if (!user) {
-      return
+    try {
+        const user = await getUserByUsername({ username });
+        console.log('user', user);
+        if (!user) {
+            return;
+        }
+        const matchingPassword = bcrypt.compareSync(password, user.password);
+        console.log(matchingPassword);
+        if (!matchingPassword) {
+            return;
+        }
+        return user;
+    } catch (error) {
+        throw error;
     }
-    const matchingPassword = bcrypt.compareSync(password, user.password)
-    console.log(matchingPassword)
-    if (!matchingPassword) {
-      return
-    }
-    return user;
-  } catch (error) {
-    throw error;
-  }
 }
 
 // goal: create a new link that tags can be add to
 // input: takes in parameters of url, title, clicks, comments, date
 // output: returns a new link
 
-async function createLink({ url, title, clicks, comments, date, tags = [] }) {
-  try {
-    const { rows } = await client.query(`insert into links (url, title,clicks, comments, date)
-            VALUES($1, $2, $3, $4, $5)
+async function createLink({ creatorId, url, title, comments, tags = [] }) {
+    let today = new Date();
+    let date = today.getFullYear()  + '/' + today.getDate() + '/' + (today.getMonth()+1) 
+    console.log('url =', url);
+    
+    try {
+        const { rows } = await client.query(`
+            INSERT into links ("creatorId", url, title, clicks, comments, date)
+            VALUES($1, $2, $3, $4, $5, $6)
             ON CONFLICT (url) DO NOTHING
-            RETURNING *;`, [url, title, clicks, comments, date])
+            RETURNING *;
+        `, [creatorId, url, title, 0, comments, date] );
 
-    if (tags.length > 0) {
-      tags.forEach(async function (tag) {
-        const tagged = await createTag(tag);
-        console.log('this is the tag', tagged);
-      })
+        if (tags.length > 0) {
+            tags.forEach(async function (tag) {
+                const tagged = await createTag(tag);
+                console.log('this is the tag', tagged);
+            });
+        }
+        return rows;
+    } catch (error) {
+        throw error;
     }
-    return rows
-  }
-  catch (error) {
-    throw error
-  }
 }
 
 // goal: update the link
@@ -112,26 +132,30 @@ async function createLink({ url, title, clicks, comments, date, tags = [] }) {
 // debt: need to also update tags
 
 async function updateLink(linkId, fields = {}) {
-  const setString = Object.keys(fields).map(
-    (key, index) => `"${key}"=$${index + 1}`
-  ).join(', ');
+    const setString = Object.keys(fields)
+        .map((key, index) => `"${key}"=$${index + 1}`)
+        .join(', ');
 
-  if (setString.length === 0) {
-    return;
-  }
+    if (setString.length === 0) {
+        return;
+    }
 
-  try {
-    const { rows: [link] } = await client.query(`
+    try {
+        const {
+            rows: [link],
+        } = await client.query(
+            `
             UPDATE links
             SET ${setString}
             WHERE id=${linkId}
             RETURNING *;
-        `, Object.values(fields)
-    );
-    return getAllLinks(linkId);
-  } catch (error) {
-    throw error;
-  }
+        `,
+            Object.values(fields)
+        );
+        return getAllLinks(linkId);
+    } catch (error) {
+        throw error;
+    }
 }
 
 // goal: get a list of all links
@@ -139,28 +163,28 @@ async function updateLink(linkId, fields = {}) {
 // output: returns an array of objects
 
 async function getAllLinks(linkId = null) {
-  try {
-    const { rows: links } = await client.query(`
+    try {
+        const { rows: links } = await client.query(`
             SELECT * 
             FROM links
             ${linkId ? `WHERE id = ${linkId};` : ';'}
       `);
-    await Promise.all(links.map(
-      async function (link) {
-        const { rows: tagsArr } = await client.query(`
+        await Promise.all(
+            links.map(async function (link) {
+                const { rows: tagsArr } = await client.query(`
                 SELECT (tags.title)
                 FROM tags
                 JOIN links_tags ON tags.id = links_tags."tagId"
                 WHERE links_tags."linkId" = ${link.id};
               `);
-        console.log('tags array: ', tagsArr);
-        link.tags = tagsArr;
-      }
-    ));
-    return links;
-  } catch (error) {
-    throw error;
-  }
+                console.log('tags array: ', tagsArr);
+                link.tags = tagsArr;
+            })
+        );
+        return links;
+    } catch (error) {
+        throw error;
+    }
 }
 
 // goal: create a new tag that can be added to links
@@ -168,17 +192,20 @@ async function getAllLinks(linkId = null) {
 // output: returns a new tag
 
 async function createTag(title) {
-  try {
-    const { rows } = await client.query(`
+    try {
+        const { rows } = await client.query(
+            `
             INSERT INTO tags(title)
             VALUES($1)
             ON CONFLICT (title) DO NOTHING
             RETURNING *;
-        `, [title])
-    return rows
-  } catch (error) {
-    throw error
-  }
+        `,
+            [title]
+        );
+        return rows;
+    } catch (error) {
+        throw error;
+    }
 }
 
 // goal: adds a tag to a link
@@ -186,18 +213,22 @@ async function createTag(title) {
 // output: makes a connection between the link and tag
 
 async function addTagToLink(linkId, tagId) {
-  try {
-    const { rows: [joint] } = await client.query(`
+    try {
+        const {
+            rows: [joint],
+        } = await client.query(
+            `
         INSERT INTO links_tags ("linkId", "tagId")
         VALUES ($1, $2)
         ON CONFLICT ("linkId", "tagId") DO NOTHING
         RETURNING *;
-    `, [linkId, tagId]
-    );
-    return joint;
-  } catch (error) {
-    throw error;
-  }
+    `,
+            [linkId, tagId]
+        );
+        return joint;
+    } catch (error) {
+        throw error;
+    }
 }
 
 // goal: get a list of links by tag name
@@ -205,56 +236,60 @@ async function addTagToLink(linkId, tagId) {
 // output: an array of links associated with the tag
 
 async function getLinksByTagName(tagName) {
-  try {
-    const links = await getAllLinks();
+    try {
+        const links = await getAllLinks();
 
-    const { rows: [tagId] } = await client.query(`
+        const {
+            rows: [tagId],
+        } = await client.query(
+            `
         SELECT id
         FROM tags
         WHERE title=$1;
-    `, [tagName]
-    );
+    `,
+            [tagName]
+        );
 
-    const { rows: linkArray } = await client.query(`
+        const { rows: linkArray } = await client.query(
+            `
           SELECT (links.id)
           FROM links
           JOIN links_tags ON links.id = links_tags."linkId"
           WHERE links_tags."tagId" = $1;
-        `, [tagId.id]
-    );
+        `,
+            [tagId.id]
+        );
 
-    console.log('this is the tagId', tagId);
-    console.log('this is the tagName', tagName);
+        console.log('this is the tagId', tagId);
+        console.log('this is the tagName', tagName);
 
-
-    const { requestedLinks } = await Promise.all(linkArray.map(
-      async function (arrayItem) {
-        try {
-          return await getAllLinks(arrayItem.id);
-        } catch (error) {
-          throw error;
-        }
-      }
-    ));
-    return requestedLinks;
-  } catch (error) {
-    throw error;
-  }
-
+        const { requestedLinks } = await Promise.all(
+            linkArray.map(async function (arrayItem) {
+                try {
+                    return await getAllLinks(arrayItem.id);
+                } catch (error) {
+                    throw error;
+                }
+            })
+        );
+        return requestedLinks;
+    } catch (error) {
+        throw error;
+    }
 }
 
 module.exports = {
-  client,
-  getAllUsers,
-  createUser,
-  getUser,
-  getUserByUsername,
-  getAllLinks,
-  createLink,
-  updateLink,
-  createTag,
-  addTagToLink,
-  getLinksByTagName,
-  getUserById
-  // db methods
-}
+    client,
+    getAllUsers,
+    createUser,
+    getUser,
+    getUserByUsername,
+    getAllLinks,
+    createLink,
+    updateLink,
+    createTag,
+    addTagToLink,
+    getLinksByTagName,
+    getUserById,
+    // db methods
+};
