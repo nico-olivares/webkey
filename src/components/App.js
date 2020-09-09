78312c310660742dcead5d636ef6c1b9736f4376
causey904.bc <your_email@real_email.edu>
2020-09-08 21:19:39 -0400

    adding initial react routes on register and links

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Auth from '../pages/Auth'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { getSomething } from '../api';
+import Links from '../pages/Links';
 
 const App = () => {
     const [message, setMessage] = useState('');
@@ -20,6 +23,12 @@ const App = () => {
     return (
         <div className='App'>
             <>
+                <Router>
+                    <Link className="nav-link" to="/auth">Create Account or login</Link>
+                    <Link to="/" >This is the links view</Link>
+                    <Route path='/auth' exact render={() => <Auth />} />
+                    <Route path='/' exact render={() => <Links />}>Home</Route>
+                </Router>
                 <Header />
                 <Footer />
             </>
