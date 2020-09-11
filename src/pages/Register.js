f474fb00806176ae53fb287e56f4e0619a210def
Nicolas Olivares <nolivaresr@hotmail.com>
2020-09-11 11:22:29 -0400

    registration mostly done, but cant connect to back end yet

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,7 +3,9 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Link, redirect } from "react-router-dom";
 
-import Main from "../components/Main";
+import { register } from "../api/index";
+
+// import Main from "../components/Main";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/esm/Row";
@@ -11,46 +13,34 @@ import Col from "react-bootstrap/esm/Col";
 import Form from "react-bootstrap/esm/Form";
 import Button from "react-bootstrap/Button";
 
-function Register({ newUser, setNewUser }) {
-    console.log("newUSer", newUser);
-    let TOKEN_KEY = localStorage.getItem("TOKEN_KEY");
-    let user;
+function Register({ user, setUser }) {
+    let username;
     let password1;
     let password2;
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log("this is the register event", event);
-        console.log("this is the user...", user);
+        console.log("this is the username...", username);
         console.log("this is the password1...", password1);
         console.log("this is the password2...", password2);
 
-        //check the password if they match create user
-        // axios call gets the token
-        // once theres a user
-        // check for token
-        //successful register
-        // {...user}
-        setNewUser(user);
+        if (password1 === password2) {
+            register({ username, password1 }).then((newUser) => {
+                setUser(newUser).catch((error) => {
+                    throw error;
+                });
+            });
+        }
     };
 
     const handleUser = (event) => {
-        user = event.target.value;
-        return user;
-        // storeCurrentUser(selectedUser); // NEW
-        // setNewUser(selectedUser);
+        username = event.target.value;
     };
     const handlePassword = (event) => {
         password1 = event.target.value;
-        return password1;
-        // storeCurrentUser(selectedUser); // NEW
-        // setNewUser(selectedUser);
     };
     const handlePassword2 = (event) => {
         password2 = event.target.value;
-        return password2;
-        // storeCurrentUser(selectedUser); // NEW
-        // setNewUser(selectedUser);
     };
 
     return (
@@ -58,13 +48,7 @@ function Register({ newUser, setNewUser }) {
             <Form onSubmit={handleSubmit}>
                 <Form.Group>
                     <Form.Label htmlFor="username">Username</Form.Label>
-                    <Form.Control
-                        id="username"
-                        name="username"
-                        type="text"
-                        onChange={handleUser}
-                        value={newUser.username}
-                    />
+                    <Form.Control id="username" name="username" type="text" onChange={handleUser} />
                 </Form.Group>
 
                 <Form.Group>
