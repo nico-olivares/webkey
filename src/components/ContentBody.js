b46094da1755045c6a1fed366eb217a2efb23341
coyotewind <coyoteswind@gmail.com>
2020-09-12 14:00:58 -0400

    added form to ContentBody component

--- src/components/ContentBody.js
+++ src/components/ContentBody.js
@@ -6,19 +6,60 @@ import './ContentBody.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function ContentBody({ links }) {
     return (
         <Container id="content-body" fluid={true}>
             <Row>
                 <Col>
-                    <div class="content-links">
-                        <p>this is showing</p>
-                        {
-                            links.map((link, index) => {
-                                return <div key={index}>{link.title} {link.url} {link.tags}</div>
-                            })
-                        }
+                    <div className="content-links">
+                        <Accordion defaultActiveKey="1">
+                            {
+                                links.map((link, index) => {
+                                    return (
+                                        <Card key={index}>
+                                            <Card.Header>
+                                                <div className="title">{link.title}</div>
+                                                <Accordion.Toggle className="btn-toggle" as={Button} variant="link" eventKey={link.id}>
+                                                    <span>Toggle</span>
+                                                </Accordion.Toggle>
+                                            </Card.Header>
+                                            <Accordion.Collapse eventKey={link.id}>
+                                                <Card.Body>
+
+                                                    <Form id="add-link" onSubmit="{ }">
+                                                        <Form.Group>
+                                                            <Form.Label htmlFor="link-url">URL</Form.Label>
+                                                            <Form.Control value={link.url} id="link-url" name="link-url" type="text" onChange="{}" />
+                                                        </Form.Group>
+                                                        <Form.Group>
+                                                            <Form.Label htmlFor="link-title">Title</Form.Label>
+                                                            <Form.Control value={link.title} id="link-title" name="link-title" type="text" onChange="{}" />
+                                                        </Form.Group>
+                                                        <Form.Group>
+                                                            <Form.Label>Enter Description</Form.Label>
+                                                            <Form.Control 
+                                                                value={link.description} id="link-desc" name="link-desc" as="textarea" rows="4"
+                                                                onChange="{}"
+                                                            />
+                                                        </Form.Group>
+                                                        <Form.Group>
+                                                            <Form.Label htmlFor="link-tags">Description</Form.Label>
+                                                            <Form.Control value={link.tags} id="link-tags" name="link-tags" type="text" onChange="{}" />
+                                                        </Form.Group>
+                                                        <Button className="d-inline-block" variant="primary" type="submit">Save Link</Button>
+                                                    </Form>
+                                                </Card.Body>
+                                            </Accordion.Collapse>
+                                        </Card>
+                                    )
+                                })
+                            }
+                        </Accordion>
                     </div>
                 </Col>
             </Row>
