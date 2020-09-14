1e7a491c24c761a98d38c0fdba770b6f54fb8542
Nicolas Olivares <nolivaresr@hotmail.com>
2020-09-13 20:54:38 -0400

    final touches

--- src/api/index.js
+++ src/api/index.js
@@ -1,119 +1,129 @@
-import userUtil from '../utils/user'
-import axios from "axios";
+import userUtil from '../utils/user';
+import axios from 'axios';
 
 export async function getLinks() {
-    try {
-        const user = userUtil.getUserFromStorage();
-        const { data: { links } } = await axios.get("/api/links", {
-            headers: {
-                authorization: 'Bearer ' + user.token
-            }
-        });
-        return links;
-    } catch (error) {
-        throw error;
-    }
+	try {
+		const user = userUtil.getUserFromStorage();
+		const {
+			data: { links },
+		} = await axios.get('/api/links', {
+			headers: {
+				authorization: 'Bearer ' + user.token,
+			},
+		});
+		return links;
+	} catch (error) {
+		throw error;
+	}
 }
 
 export async function register({ username, password }) {
-    try {
+	try {
+		const {
+			data: { user: newUser },
+		} = await axios.post('/api/users/register', {
+			username: username,
+			password: password,
+		});
 
-        const { data: { user: newUser } } = await axios.post("/api/users/register", {
-            username: username,
-            password: password,
-        });
-
-        if (newUser) {
-            localStorage.setItem("user", JSON.stringify(newUser));
-            return newUser;
-        } else {
-            alert("Please login to access these features.");
-        }
-    } catch (error) {
-        throw error;
-    }
+		if (newUser) {
+			localStorage.setItem('user', JSON.stringify(newUser));
+			return newUser;
+		} else {
+			alert('Please login to access these features.');
+		}
+	} catch (error) {
+		throw error;
+	}
 }
 
 export async function login({ username, password }) {
-    try {
-        const { data: { user } } = await axios.post('api/users/login', {
-            username,
-            password
-        });
-        if (user) {
-            localStorage.setItem('user', JSON.stringify(user));
-            return user;
-        }
-    } catch (error) {
-        throw error;
-    }
+	try {
+		const {
+			data: { user },
+		} = await axios.post('api/users/login', {
+			username,
+			password,
+		});
+		if (user) {
+			localStorage.setItem('user', JSON.stringify(user));
+			return user;
+		}
+	} catch (error) {
+		throw error;
+	}
 }
 
-
 export async function getTags() {
-    
-
-    try {
-        const user = userUtil.getUserFromStorage();
-        const { data: tags } = await axios.get('/api/tags/usertags', {
-            headers: {
-                authorization: 'Bearer ' + user.token
-            }
-        });
-        if (tags) {
-            tags.sort((a, b) => {
-                if (a.title[0] - b.title[0] === 0) {
-                    if (a.title[1] - b.title[1] === 0 ) {
-                        return a.title[2] - b.title[2];
-                    } else {
-                        return a.title[1] - b.title[1];
-                    }
-                } else {
-                    return a.title[0] - b.title[0];
-                }
-            });
-            return tags;     
-        } else {
-            return [];
-        }
-    } catch (error) {
-        throw error;
-    }
+	try {
+		const user = userUtil.getUserFromStorage();
+		const { data: tags } = await axios.get('/api/tags/usertags', {
+			headers: {
+				authorization: 'Bearer ' + user.token,
+			},
+		});
+		if (tags) {
+			tags.sort((a, b) => {
+				if (a.title[0] - b.title[0] === 0) {
+					if (a.title[1] - b.title[1] === 0) {
+						return a.title[2] - b.title[2];
+					} else {
+						return a.title[1] - b.title[1];
+					}
+				} else {
+					return a.title[0] - b.title[0];
+				}
+			});
+			return tags;
+		} else {
+			return [];
+		}
+	} catch (error) {
+		throw error;
+	}
 }
 
 export async function addNewLink({ title, description, url, tags = [] }) {
-    try {
-        const user = userUtil.getUserFromStorage();
-        const { data: link } = await axios.post('api/links', {
-            headers: {
-                authorization: 'Bearer ' + user.token
-            },
-            body: {
-            title, description, url, tags
-        }});
-        if (link) {
-            return link
-        } else {
-            return {}
-        }
-    } catch (error) {
-        throw error
-    }
+	try {
+		const user = userUtil.getUserFromStorage();
+		const { data: link } = await axios.post('api/links', {
+			headers: {
+				authorization: 'Bearer ' + user.token,
+			},
+			body: {
+				title,
+				description,
+				url,
+				tags,
+			},
+		});
+		if (link) {
+			return link;
+		} else {
+			return {};
+		}
+	} catch (error) {
+		throw error;
+	}
 }
 
 export async function updatedLink({ id, title, date, clicks, description, url, tags = [] }) {
-    try {
-        const { data: link } = await axios.patch(`api/links/${id}`, {
-            id, title, date, clicks, description, url, tags
-        })
-        if (link) {
-
-
-            return link
-        } else {
-            return {}
-
-
-        }
-    
+	try {
+		const { data: link } = await axios.patch(`api/links/${id}`, {
+			id,
+			title,
+			date,
+			clicks,
+			description,
+			url,
+			tags,
+		});
+		if (link) {
+			return link;
+		} else {
+			return {};
+		}
+	} catch (error) {
+		throw error;
+	}
 }
--- src/components/ContentLink.js
+++ src/components/ContentLink.js
@@ -1,71 +1,103 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 import './ContentLink.css';
 
-import { updatedLink } from '../api/index'
+import { updatedLink } from '../api/index';
 
 function ContentLink({ link }) {
+	const [id, setId] = useState(link.id);
+	const [url, setUrl] = useState(link.url);
+	const [description, setDescription] = useState(link.description);
+	const [title, setTitle] = useState(link.title);
+	const [tags, setTags] = useState(link.tags.join(' '));
 
-    const [id, setId] = useState(link.id);
-    const [url, setUrl] = useState(link.url);
-    const [description, setDescription] = useState(link.description);
-    const [title, setTitle] = useState(link.title)
-    const [tags, setTags] = useState(link.tags.join(' '));
+	const submitHandler = function (event) {
+		event.preventDefault();
+		updatedLink({ id, url, title, description, tags })
+			.then((result) => {})
+			.catch((error) => {
+				throw error;
+			});
+	};
 
-    const submitHandler = function (event) {
-        event.preventDefault();
-        updatedLink({ id, url, title, description, tags });
-    }
+	const onChange = (update) => (event) => {
+		event.preventDefault();
+		update(event.target.value);
+	};
 
-    const onChange = (update) => (event) => {
-        event.preventDefault();
-        update(event.target.value)
-    }
-   
-    return (
-        <Card key={link.id}>
-            <Card.Header>
-                <Card.Link className="title" href={link.url} target="_blank">{link.title}</Card.Link>
-                <Accordion.Toggle className="btn-toggle" variant="link" eventKey={link.id}>
-                    <FontAwesomeIcon icon={ faChevronDown } />
-                </Accordion.Toggle>
-            </Card.Header>
-            <Accordion.Collapse eventKey={link.id}>
-                <Card.Body>
-
-                    <Form id="edit-link" onSubmit={submitHandler}>
-                        <Form.Group className="col-md-12 col-sm-12">
-                            <Form.Label htmlFor="link-url">URL</Form.Label>
-                            <Form.Control value={url} id="link-url" name="link-url" type="text" onChange={onChange(setUrl)} />
-                        </Form.Group>
-                        <Form.Group className="col-md-12 col-sm-12">
-                            <Form.Label htmlFor="link-title">Title</Form.Label>
-                            <Form.Control value={title} id="link-title" name="link-title" type="text" onChange={onChange(setTitle)} />
-                        </Form.Group>
-                        <Form.Group className="col-md-12">
-                            <Form.Label>Enter Description</Form.Label>
-                            <Form.Control value={description} id="link-desc" name="link-desc" as="textarea" rows="4" onChange={onChange(setDescription)} />
-                        </Form.Group>
-                        <Form.Group className="col-md-12">
-                            <Form.Label htmlFor="link-tags">Tags</Form.Label>
-                            <Form.Control value={tags} id="link-tags" name="link-tags" type="text" onChange={onChange(setTags)} />
-                        </Form.Group>
-                        <Button 
-                            className="d-inline-block mt-2 mb-4 mx-3" variant="primary" type="submit"
-                        >Save Link</Button>
-                    </Form>
-
-                </Card.Body>
-            </Accordion.Collapse>
-        </Card>
-    )
+	return (
+		<Card key={link.id}>
+			<Card.Header>
+				<Card.Link className='title' href={link.url} target='_blank'>
+					{link.title}
+				</Card.Link>
+				<Accordion.Toggle className='btn-toggle' variant='link' eventKey={link.id}>
+					<FontAwesomeIcon icon={faChevronDown} />
+				</Accordion.Toggle>
+			</Card.Header>
+			<Accordion.Collapse eventKey={link.id}>
+				<Card.Body>
+					<Form id='edit-link' onSubmit={submitHandler}>
+						<Form.Group className='col-md-12 col-sm-12'>
+							<Form.Label htmlFor='link-url'>URL</Form.Label>
+							<Form.Control
+								value={url}
+								id='link-url'
+								name='link-url'
+								type='text'
+								onChange={onChange(setUrl)}
+							/>
+						</Form.Group>
+						<Form.Group className='col-md-12 col-sm-12'>
+							<Form.Label htmlFor='link-title'>Title</Form.Label>
+							<Form.Control
+								value={title}
+								id='link-title'
+								name='link-title'
+								type='text'
+								onChange={onChange(setTitle)}
+							/>
+						</Form.Group>
+						<Form.Group className='col-md-12'>
+							<Form.Label>Enter Description</Form.Label>
+							<Form.Control
+								value={description}
+								id='link-desc'
+								name='link-desc'
+								as='textarea'
+								rows='4'
+								onChange={onChange(setDescription)}
+							/>
+						</Form.Group>
+						<Form.Group className='col-md-12'>
+							<Form.Label htmlFor='link-tags'>Tags</Form.Label>
+							<Form.Control
+								value={tags}
+								id='link-tags'
+								name='link-tags'
+								type='text'
+								onChange={onChange(setTags)}
+							/>
+						</Form.Group>
+						<Button
+							className='d-inline-block mt-2 mb-4 mx-3'
+							variant='primary'
+							type='submit'
+						>
+							Save Link
+						</Button>
+					</Form>
+				</Card.Body>
+			</Accordion.Collapse>
+		</Card>
+	);
 }
 
 export default ContentLink;
