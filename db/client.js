36d748e9799e38ec75da7b6a33ce7d2b029ff0c9
Nicolas Olivares <nolivaresr@hotmail.com>
2021-03-27 21:27:22 -0400

    final comment. now it works

--- db/client.js
+++ db/client.js
@@ -3,6 +3,13 @@ const { Client } = require('pg');
 const DB_NAME = 'webkey';
 
 const DB_URL = process.env.DATABASE_URL || `postgres://localhost:5432/webkey`;
+
+
+/*
+This code will work with heroku by establishing an ssl connection.
+This code won't work on the other hand in the localhost. For that purpose the code needs to be changed to:
+const client = new Client(DB_URL);
+*/
 const client = new Client({
     connectionString: DB_URL,
     ssl: {
